ee4f3dbcf9c6669424b8fadb7ada083c4fe41d03
nokcha11 <lie8220@naver.com>
2024-06-21 23:32:49 +0900

    커밋,푸시 연동 테스트

---
+++ Java_teach/src/Java_teach_package/Operator03_2.java
@@ -0,0 +1,13 @@
+package Java_teach_package;
+
+public class Operator03_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 21 보다 크거나 같고(이상), 59 보다 작은(미만)
+		int num = 59;
+		System.out.println((num >= 21) && (num < 59));
+	}
+
+}
---
+++ Java_teach/src/Java_teach_package/test_class.java
@@ -0,0 +1,10 @@
+package Java_teach_package;
+
+public class test_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
